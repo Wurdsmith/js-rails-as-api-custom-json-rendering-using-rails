3a540ce7457ce6a42ce36e405d819e1cce8226af
Wurdsmith <wurdsmithing@gmail.com>
2021-04-23 07:43:42 -0500

    commit tree 0f8855b4c823469bebf68f376cc8225ff525305e

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,20 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render json: @birds
+    birds = Bird.all
+    #only: works for an array of hashes, unlike slice, etc. You can also use except:.
+    render json: birds, only: [:id, :name, :species]
   end
+
+  def show
+  # Alternatively...
+  #bird = Bird.find_by(id: params[:id])
+  #render json: bird.slice(:id, :name, :species)
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      render json: { id: bird.id, name: bird.name, species: bird.species }
+    else
+      render json: { message: 'Bird not found' }
+    end
+  end 
+
 end
